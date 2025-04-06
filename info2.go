6b5d98235676a7e3a91e8f7c81484703da11c42a
Juillard Thibault <thibault.juillard@ecole2600.com>
2025-04-06 11:07:21 +0200

    simple parser for info2files

---
+++ info2.go
@@ -0,0 +1,54 @@
+package info2parser
+
+import (
+	"encoding/binary"
+	"fmt"
+	"time"
+	"unicode/utf16"
+)
+
+type Info2 struct {
+	Header         uint64
+	FileSize       uint64
+	DeletionTime   int64
+	FileNameLength uint32
+	OriginalPath   string
+}
+
+func FiletimeToTime(filetime int64) time.Time {
+	return time.Unix(0, (filetime-116444736000000000)*100)
+}
+
+func decodeUTF16LE(b []byte) string {
+	u16 := make([]uint16, len(b)/2)
+	for i := 0; i < len(u16); i++ {
+		u16[i] = binary.LittleEndian.Uint16(b[i*2:])
+	}
+	for i, v := range u16 {
+		if v == 0 {
+			u16 = u16[:i]
+			break
+		}
+	}
+	return string(utf16.Decode(u16))
+}
+
+func Parse(data []byte) (Info2, error) {
+	if len(data) < 28 {
+		return Info2{}, fmt.Errorf("file too small to be a valid $I* file")
+	}
+
+	header := binary.LittleEndian.Uint64(data[0:8])
+	fileSize := binary.LittleEndian.Uint64(data[8:16])
+	deletionTimeRaw := int64(binary.LittleEndian.Uint64(data[16:24]))
+	fileNameLength := binary.LittleEndian.Uint32(data[24:28])
+	originalPath := decodeUTF16LE(data[28:])
+
+	return Info2{
+		Header:         header,
+		FileSize:       fileSize,
+		DeletionTime:   deletionTimeRaw,
+		FileNameLength: fileNameLength,
+		OriginalPath:   originalPath,
+	}, nil
+}
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gobelinor/info2parser"
+)
+
+func main() {
+	filePath := flag.String("file", "", "Path to the $I file to parse")
+	flag.Parse()
+
+	if *filePath == "" {
+		fmt.Println("Please provide the path to the $I file using the -file flag.")
+		os.Exit(1)
+	}
+
+	data, err := os.ReadFile(*filePath)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	info2, err := info2parser.Parse(data)
+	if err != nil {
+		fmt.Printf("Error parsing file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("==== $I FILE ====")
+	fmt.Printf("Header       : %d\n", info2.Header)
+	fmt.Printf("FileSize       : %d octets\n", info2.FileSize)
+	fmt.Printf("DeletionTime   : %s\n", info2parser.filetimeToTime(info2.DeletionTime).Format(time.RFC3339))
+	fmt.Printf("FileNameLength : %d\n", info2.FileNameLength)
+	fmt.Printf("OriginalPath : %s\n", info2.OriginalPath)
+}
